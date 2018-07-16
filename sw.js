555bd7e173e9efdb9c540486f86ec89250fce65e
Iulian <savin.iulian90@gmail.com>
2018-07-17 00:11:39 +0300

    work so far

---
+++ sw.js
@@ -0,0 +1,44 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('restaurant').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/restaurant.html',
+        '/restaurant.html?id=1',
+        '/restaurant.html?id=2',
+        '/restaurant.html?id=3',
+        '/restaurant.html?id=4',
+        '/restaurant.html?id=5',
+        '/restaurant.html?id=6',
+        '/restaurant.html?id=7',
+        '/restaurant.html?id=8',
+        '/restaurant.html?id=9',
+        '/restaurant.html?id=10',
+        '/css/styles.css',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/img/10.jpg',
+        '/img/favicon.ico',
+        '/js/dbhelper.js',
+        '/js/main.js',
+        '/js/restaurant_info.js']);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
