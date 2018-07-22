a2fb79ba72766ce3162fdcc7ff27773d776cd9b2
Iulian <savin.iulian90@gmail.com>
2018-07-22 22:10:08 +0300

    added gulp-sass -> write scss and compile to compressed css

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,27 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 
-gulp.task('styles', function(done){
-  gulp.src('sass/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./css'));
-    done();
+// gulp.task('styles', function() {
+//     gulp.src('sass/**/*.scss')
+//         .pipe(sass().on('error', sass.logError))
+//         .pipe(gulp.dest('./css/'));
+// });
+
+
+
+gulp.task('style', () => {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass({ outputStyle: "compressed"})
+        .on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
 });
+
+
+// gulp.task('serve', ['style'], () => {
+//     gulp.watch('sass/**/*.scss', ['style']);
+// });
+
+
+gulp.task('default', gulp.series(gulp.parallel(['style']), function(){
+  gulp.watch('sass/**/*.scss', ['style']);
+}))
